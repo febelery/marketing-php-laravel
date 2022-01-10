b693532fd7db781ea19a5e1520f7e5993d17c738
ross <rossross>
2022-01-10 14:39:44 +0800

    chore: change setting table field

--- app/Forms/Components/SettingForm.php
+++ app/Forms/Components/SettingForm.php
@@ -40,61 +40,61 @@ class SettingForm extends Forms\Components\Field
         return [
             Forms\Components\Placeholder::make('使用平台'),
             Forms\Components\Grid::make(4)->schema([
-                Forms\Components\Toggle::make('is_wechat')
+                Forms\Components\Toggle::make('allow_wechat')
                     ->label('微信')
                     ->helperText('微信投票')
                     ->default(true),
-                Forms\Components\Toggle::make('is_wechat_app')
+                Forms\Components\Toggle::make('allow_wechat_app')
                     ->label('小程序')
                     ->helperText('微信小程序投票')
                     ->disabled()
                     ->default(false),
-                Forms\Components\Toggle::make('is_wechat_web')
+                Forms\Components\Toggle::make('allow_wechat_web')
                     ->label('网页')
                     ->helperText('微信网页投票')
                     ->disabled()
                     ->default(false),
-                Forms\Components\Toggle::make('is_anon')
+                Forms\Components\Toggle::make('allow_anon')
                     ->label('匿名')
                     ->disabled()
                     ->helperText('匿名投票')
                     ->default(false),
             ]),
             Forms\Components\Grid::make(4)->schema([
-                Forms\Components\Toggle::make('is_cgxw')
+                Forms\Components\Toggle::make('allow_cgxw')
                     ->label('APP')
                     ->reactive()
                     ->helperText('川观新闻投票')
                     ->default(true),
-                Forms\Components\Toggle::make('is_bind_phone')
+                Forms\Components\Toggle::make('bind_phone')
                     ->label('绑定手机')
                     ->helperText('用手机号登录')
-                    ->when(fn(callable $get) => $get('is_cgxw') === true)
+                    ->when(fn(callable $get) => $get('allow_cgxw') === true)
                     ->default(true),
-                Forms\Components\Toggle::make('is_bind_wechat')
+                Forms\Components\Toggle::make('bind_wechat')
                     ->label('绑定微信')
                     ->helperText('绑定微信')
-                    ->when(fn(callable $get) => $get('is_cgxw') === true)
+                    ->when(fn(callable $get) => $get('allow_cgxw') === true)
                     ->default(false),
             ]),
             Forms\Components\Grid::make(4)->schema([
-                Forms\Components\Toggle::make('is_share')
+                Forms\Components\Toggle::make('allow_share')
                     ->label('分享')
                     ->helperText('是否可以分享')
                     ->default(false)
                     ->reactive(),
                 Forms\Components\TextInput::make('share_title')
                     ->label('分享标题')
-                    ->required(fn(callable $get) => $get('is_share') === true)
-                    ->when(fn(callable $get) => $get('is_share') === true),
+                    ->required(fn(callable $get) => $get('allow_share') === true)
+                    ->when(fn(callable $get) => $get('allow_share') === true),
                 Forms\Components\TextInput::make('share_desc')
                     ->label('分享描述')
-                    ->required(fn(callable $get) => $get('is_share') === true)
-                    ->when(fn(callable $get) => $get('is_share') === true),
+                    ->required(fn(callable $get) => $get('allow_share') === true)
+                    ->when(fn(callable $get) => $get('allow_share') === true),
                 Forms\Components\FileUpload::make('share_image')
                     ->label('分享图片')
-                    ->required(fn(callable $get) => $get('is_share') === true)
-                    ->when(fn(callable $get) => $get('is_share') === true),
+                    ->required(fn(callable $get) => $get('allow_share') === true)
+                    ->when(fn(callable $get) => $get('allow_share') === true),
             ]),
         ];
     }
