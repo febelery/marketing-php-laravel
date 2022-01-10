70e9f60890fb6e8d1008172c18578dc8d7ab8492
ross <rossross>
2022-01-10 14:36:55 +0800

    chore: change setting table field

--- database/migrations/2021_12_22_155840_settings.php
+++ database/migrations/2021_12_22_155840_settings.php
@@ -18,14 +18,14 @@ class Settings extends Migration
             $table->unsignedInteger('settingable_id');
             $table->string('settingable_type');
 
-            $table->boolean('is_cgxw')->default(true)->comment('川观新闻限制');
-            $table->string('is_bind_phone')->nullable()->comment('绑定电话');
-            $table->string('is_bind_wechat')->nullable()->comment('绑定微信');
-            $table->boolean('is_wechat')->default(true)->comment('微信限制');
-            $table->boolean('is_wechat_app')->default(false)->comment('微信小程序限制');
-            $table->boolean('is_wechat_web')->default(false)->comment('微信网站限制');
-            $table->boolean('is_anon')->default(false)->comment('匿名投票');
-            $table->boolean('is_share')->default(true)->comment('是否可以分享');
+            $table->boolean('allow_cgxw')->default(true)->comment('川观新闻限制');
+            $table->string('bind_phone')->nullable()->comment('绑定电话');
+            $table->string('bind_wechat')->nullable()->comment('绑定微信');
+            $table->boolean('allow_wechat')->default(true)->comment('微信限制');
+            $table->boolean('allow_wechat_app')->default(false)->comment('微信小程序限制');
+            $table->boolean('allow_wechat_web')->default(false)->comment('微信网站限制');
+            $table->boolean('allow_anon')->default(false)->comment('匿名投票');
+            $table->boolean('allow_share')->default(true)->comment('是否可以分享');
             $table->string('share_title')->nullable()->comment('分享标题');
             $table->string('share_desc')->nullable()->comment('分享描述');
             $table->string('share_image')->nullable()->comment('分享图片');
